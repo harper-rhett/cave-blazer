d22fd3ee7f1b990361aef6dc52521570c06845b4
Harper Rhett <harperrhett@gmail.com>
2025-10-05 12:05:56 -0500

    movement!

--- CaveBlazer/CaveBlazer/Player.cs
+++ CaveBlazer/CaveBlazer/Player.cs
@@ -1,17 +1,30 @@
 ﻿using HarpEngine;
 using HarpEngine.Graphics;
+using HarpEngine.Input;
 using System.Numerics;
 
 internal class Player : Entity
 {
+	// General
 	private Vector2 position;
-	public Vector2 Position => position;
-
+	private Vector2 velocity;
+	private bool isGrounded;
 	private Area currentArea;
+	
+	// Texture
 	private Texture texture;
 	private int width;
 	private int height;
 	private int halfWidth;
+
+	// Settings
+	private const float gravity = 45;
+	private const float jumpForce = 30;
+	private const float walkSpeed = 10;
+	private const float floatAcceleration = 5;
+
+	// Interface
+	public Vector2 Position => position;
 
 	public Player(Scene scene, Area area, Vector2 position) : base(scene)
 	{
@@ -26,15 +39,48 @@ internal class Player : Entity
 
 	public override void Update()
 	{
-		int feetX = (int)position.X + halfWidth;
-		int feetY = (int)position.Y + height;
-		bool isGrounded = currentArea.IsWall(feetX, feetY);
+		CheckGrounded();
+		CheckJump();
 
-		if (!isGrounded) position.Y += 15f * Engine.FrameTime;
+		if (isGrounded)
+		{
+			velocity.Y = 0;
+
+			if (Keyboard.IsKeyDown(KeyboardKey.Left)) velocity.X = -walkSpeed;
+			else if (Keyboard.IsKeyDown(KeyboardKey.Right)) velocity.X = walkSpeed;
+			else velocity.X = 0;
+		}
+		else
+		{
+			velocity.Y += gravity * Engine.FrameTime;
+
+			if (Keyboard.IsKeyDown(KeyboardKey.Left)) velocity.X -= floatAcceleration * Engine.FrameTime;
+			else if (Keyboard.IsKeyDown(KeyboardKey.Right)) velocity.X += floatAcceleration * Engine.FrameTime;
+		}
+
+		position += velocity * Engine.FrameTime;
 	}
 
 	public override void Draw()
 	{
 		texture.Draw(Position, Colors.White);
+	}
+
+	private void CheckGrounded()
+	{
+		int leftFootX = (int)position.X;
+		int rightFootX = (int)position.X + width;
+		int feetY = (int)position.Y + height;
+		isGrounded = currentArea.IsWall(leftFootX, feetY) || currentArea.IsWall(rightFootX, feetY);
+	}
+
+	private void CheckJump()
+	{
+		bool didJump = isGrounded && Keyboard.IsKeyPressed(KeyboardKey.Space);
+		if (didJump)
+		{
+			isGrounded = false;
+			velocity.Y -= jumpForce;
+		}
 	}
 }
