d7089863c95631cd872b4d5a99f1536754e4523e
Harper Rhett <harperrhett@gmail.com>
2025-11-22 13:08:17 -0600

    new tileset changes

--- CaveBlazer/CaveBlazer/GameScene.cs
+++ CaveBlazer/CaveBlazer/GameScene.cs
@@ -12,7 +12,7 @@ public class GameScene : Scene
 
 	public GameScene() : base(Colors.SkyBlue)
 	{
-		LDTKImporter importer = new("world.ldtk", 16);
+		LDTKImporter importer = new("world.ldtk", 8);
 		World = AddEntity(importer.GenerateWorld());
 		player = AddEntity(new Player(this, importer.SpawnArea, importer.SpawnPosition));
 		
