34e49e986704b212296e6ecdf04fb0ec902cbd1a
Harper Rhett <harperrhett@gmail.com>
2025-12-17 16:17:46 -0600

    dialogue!

--- CaveBlazer/CaveBlazer/DialogueEntity.cs
+++ CaveBlazer/CaveBlazer/DialogueEntity.cs
@@ -12,12 +12,23 @@ public class DialogueEntity : Entity
 	private Texture texture;
 	private GameScene gameScene;
 	private string text;
-	private const int fontSize = 10;
-	private const int fontSpacing = 1;
+	private const int fontSize = 14;
+	private const int fontSpacing = 0;
 	private Vector2 textSize;
 	private Vector2 textPosition;
-	private Vector2 borderSize = new(2, 2);
+	private const int leftBorder = 1;
+	private const int rightBorder = 0;
+	private const int topBorder = -4;
+	private const int bottomBorder = -1;
 	private Vector2 boxSize;
+	private Vector2 boxPosition;
+	private static Font font;
+
+	static DialogueEntity()
+	{
+		font = Font.Load("fonts/Tiny5-Regular.fnt");
+		font.Texture.SetFilter(TextureFilter.Point);
+	}
 
 	public DialogueEntity(LDTKEntity ldtkEntity, GameScene gameScene)
 	{
@@ -33,10 +44,11 @@ public class DialogueEntity : Entity
 		int halfWidth = texture.Width / 2;
 
 		text = ldtkEntity.FieldsByID["text"].Value;
-		textSize = Text.MeasureSize(Font.Default, text, fontSize, fontSpacing);
+		textSize = Text.MeasureSize(font, text, fontSize, fontSpacing);
 		int halfTextWidth = (textSize.X / 2f).Floored();
-		textPosition = position + new Vector2(-halfTextWidth + halfWidth, -fontSize * 2);
-		boxSize = textSize + borderSize;
+		textPosition = position + new Vector2(-halfTextWidth + halfWidth, -textSize.Y);
+		boxSize = textSize + new Vector2(leftBorder + rightBorder, topBorder + bottomBorder);
+		boxPosition = textPosition + new Vector2(-leftBorder, -topBorder);
 	}
 
 	public override void OnDraw()
@@ -45,9 +57,8 @@ public class DialogueEntity : Entity
 
 		if (gameScene.Player.IntersectsWithRectangle(rectangle))
 		{
-			Vector2 boxOffset = -borderSize / 2f;
-			Primitives.DrawRectangle(textPosition + boxOffset, boxSize, Colors.White.SetAlpha(0.5f));
-			Text.Draw(Font.Default, text, textPosition, Vector2.Zero, 0, fontSize, fontSpacing, Colors.Black);
+			Primitives.DrawRectangle(boxPosition, boxSize, Colors.White.SetAlpha(0.5f));
+			Text.Draw(font, text, textPosition, Vector2.Zero, 0, fontSize, fontSpacing, Colors.Black);
 		}
 	}
 }
