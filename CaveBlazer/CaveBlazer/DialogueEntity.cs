d17ff3afb2f47e274a3a8e634f59a1455ce5e688
Harper Rhett <harperrhett@gmail.com>
2025-12-17 13:06:48 -0600

    dialogue sorta working

--- CaveBlazer/CaveBlazer/DialogueEntity.cs
+++ CaveBlazer/CaveBlazer/DialogueEntity.cs
@@ -1,6 +1,7 @@
 ﻿using HarpEngine;
 using HarpEngine.Graphics;
 using HarpEngine.Tiles;
+using HarpEngine.Utilities;
 using HarpEngineLDTKImporter;
 using System.Numerics;
 
@@ -11,6 +12,12 @@ public class DialogueEntity : Entity
 	private Texture texture;
 	private GameScene gameScene;
 	private string text;
+	private const int fontSize = 10;
+	private const int fontSpacing = 1;
+	private Vector2 textSize;
+	private Vector2 textPosition;
+	private Vector2 borderSize = new(2, 2);
+	private Vector2 boxSize;
 
 	public DialogueEntity(LDTKEntity ldtkEntity, GameScene gameScene)
 	{
@@ -23,14 +30,24 @@ public class DialogueEntity : Entity
 		if (type == "monk") texturePath = "sprites/entities/monk.png";
 		else if (type == "sign") texturePath = "sprites/entities/sign.png";
 		texture = Texture.Load(texturePath);
+		int halfWidth = texture.Width / 2;
 
 		text = ldtkEntity.FieldsByID["text"].Value;
+		textSize = Text.MeasureSize(Font.Default, text, fontSize, fontSpacing);
+		int halfTextWidth = (textSize.X / 2f).Floored();
+		textPosition = position + new Vector2(-halfTextWidth + halfWidth, -fontSize * 2);
+		boxSize = textSize + borderSize;
 	}
 
 	public override void OnDraw()
 	{
 		texture.Draw(position, Colors.White);
 
-		if (gameScene.Player.IntersectsWithRectangle(rectangle)) Console.WriteLine(text);
+		if (gameScene.Player.IntersectsWithRectangle(rectangle))
+		{
+			Vector2 boxOffset = -borderSize / 2f;
+			Primitives.DrawRectangle(textPosition + boxOffset, boxSize, Colors.White.SetAlpha(0.5f));
+			Text.Draw(Font.Default, text, textPosition, Vector2.Zero, 0, fontSize, fontSpacing, Colors.Black);
+		}
 	}
 }
